9dcaaccb860fcaba4a6c9068562ab3279be04c82
Samira Milne <milne.samira@GMAIL.COM>
2021-09-19 21:30:22 -0500

    added new camp form

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,7 +1,11 @@
-import classes from './Details.module.css'
+import classes from "./Details.module.css";
 
-const Details=()=>{
-    return(<div>Details</div>)
-}
+const Details = () => {
+  return (
+    <div className={classes.container}>
+  Details
+    </div>
+  );
+};
 
 export default Details;
--- src/pages/NewCamp.tsx
+++ src/pages/NewCamp.tsx
@@ -1,6 +1,40 @@
 import classes from "./NewCamp.module.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-const NewCamp = () => {
-  return <div>NewCamp</div>;
+const Details = () => {
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.h1}>New CampGround</h1>
+      <Form className={classes.form}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Location</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Campground price</Form.Label>
+          <Form.Control type="number" />
+        </Form.Group>
+        <Form.Group className="mb-4" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Description</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Form.Group controlId="formFile" className="mb-4">
+          <Form.Control type="file" />
+        </Form.Group>
+        <div className={classes.button}>
+          <Button variant="success" size="lg" type="button" className="mb-3">
+            Add campground
+          </Button>
+        </div>
+
+      </Form>
+    </div>
+  );
 };
-export default NewCamp;
+
+export default Details;
