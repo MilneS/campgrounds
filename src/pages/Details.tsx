de71ced1776f19596a38b7cc51e82ec3fd576f04
Samira Milne <milne.samira@GMAIL.COM>
2021-09-19 22:19:36 -0500

    details card

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,9 +1,26 @@
 import classes from "./Details.module.css";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+import LoginPic from '../utils/LoginPic.jpeg'
 
 const Details = () => {
   return (
     <div className={classes.container}>
-  Details
+        <Card style={{ width: "40rem" }} className={classes.card}>
+        <Card.Img variant="top" src={LoginPic} />
+          <Card.Body>
+            <Card.Title className={classes.title}>Card Title</Card.Title>
+            <Card.Text className={classes.DescrAuthPrice}>
+              Description
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem className={classes.location}>Location</ListGroupItem>
+            <ListGroupItem className={classes.DescrAuthPrice}>Submitted by</ListGroupItem>
+            <ListGroupItem className={classes.DescrAuthPrice}>$Price/night</ListGroupItem>
+          </ListGroup>
+        </Card>
     </div>
   );
 };
