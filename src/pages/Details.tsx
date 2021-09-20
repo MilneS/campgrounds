de6923661ab185733b9bc7fad1856e94cf0f6854
Samira Milne <milne.samira@GMAIL.COM>
2021-09-20 16:56:00 -0500

    details review

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -2,13 +2,15 @@ import classes from "./Details.module.css";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
-import LoginPic from '../utils/LoginPic.jpeg'
+import Form from "react-bootstrap/Form";
+import LoginPic from "../utils/LoginPic.jpeg";
 
 const Details = () => {
   return (
     <div className={classes.container}>
-        <Card style={{ width: "40rem" }} className={classes.card}>
-        <Card.Img variant="top" src={LoginPic} />
+      <div>
+        <Card style={{ width: "40rem" }}>
+          <Card.Img variant="top" src={LoginPic} />
           <Card.Body>
             <Card.Title className={classes.title}>Card Title</Card.Title>
             <Card.Text className={classes.DescrAuthPrice}>
@@ -17,10 +19,24 @@ const Details = () => {
           </Card.Body>
           <ListGroup className="list-group-flush">
             <ListGroupItem className={classes.location}>Location</ListGroupItem>
-            <ListGroupItem className={classes.DescrAuthPrice}>Submitted by</ListGroupItem>
-            <ListGroupItem className={classes.DescrAuthPrice}>$Price/night</ListGroupItem>
+            <ListGroupItem className={classes.DescrAuthPrice}>
+              Submitted by
+            </ListGroupItem>
+            <ListGroupItem className={classes.DescrAuthPrice}>
+              $Price/night
+            </ListGroupItem>
           </ListGroup>
         </Card>
+      </div>
+      <div className={classes.reviewContainer}>
+        <h1 className={classes.titleReview}>Leave a review</h1>
+        <p className={classes.textareaTitle}>Review text</p>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 };
