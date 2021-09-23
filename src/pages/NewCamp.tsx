350bac9c4b7e74a433d8859626f7763199410a02
Samira Milne <milne.samira@GMAIL.COM>
2021-09-22 21:42:25 -0500

    removes controlid

--- src/pages/NewCamp.tsx
+++ src/pages/NewCamp.tsx
@@ -54,28 +54,28 @@ const Details = () => {
     <div className={classes.container}>
       <h1 className={classes.h1}>New CampGround</h1>
       <Form className={classes.form}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Title</Form.Label>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="title">Title</Form.Label>
           <Form.Control type="text" id="title" onChange={getInputDataHandler} />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Location</Form.Label>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="location">Location</Form.Label>
           <Form.Control
             type="text"
             id="location"
             onChange={getInputDataHandler}
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Campground price</Form.Label>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="price">Campground price</Form.Label>
           <Form.Control
             type="number"
             id="price"
             onChange={getInputDataHandler}
           />
         </Form.Group>
-        <Form.Group className="mb-4" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Description</Form.Label>
+        <Form.Group className="mb-4">
+          <Form.Label htmlFor="description">Description</Form.Label>
           <Form.Control
             as="textarea"
             rows={3}
@@ -83,7 +83,7 @@ const Details = () => {
             onChange={getInputDataHandler}
           />
         </Form.Group>
-        <Form.Group controlId="formFile" className="mb-4">
+        <Form.Group className="mb-4">
           <Form.Control type="file" id="image" onChange={getInputDataHandler} />
         </Form.Group>
         <div className={classes.button}>
