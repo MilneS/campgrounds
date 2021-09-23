1fd1b4b0a453aa0542b766e398036383762b2bf8
Samira Milne <milne.samira@GMAIL.COM>
2021-09-22 21:17:01 -0500

    get newCamp data

--- src/pages/NewCamp.tsx
+++ src/pages/NewCamp.tsx
@@ -4,13 +4,25 @@ import Button from "react-bootstrap/Button";
 import { useState } from "react";
 
 const Details = () => {
-
-  const initialData = { title: "", location: "", price: "", description:"", image: ""  };
+  const initialData = {
+    title: "",
+    location: "",
+    price: "",
+    description: "",
+    image: "",
+  };
   const [inputData, setInputData] = useState(initialData);
 
+  const getInputDataHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData({ ...inputData, [e.target.id]: e.currentTarget.value });
+  };
 
+  const getFormDataHandler = (e: React.MouseEvent) => {
+    e.preventDefault();
+    newCampDataHandler();
+  };
 
-  const newCampHandler = async () => {
+  const newCampDataHandler = async () => {
     const newCampApi: any = process.env.REACT_APP_API_NEWCAMPS;
     const response = await fetch(newCampApi, {
       method: "POST",
@@ -22,6 +34,9 @@ const Details = () => {
     const data = await response.json();
     if (response.ok) {
       console.log(data);
+    } else {
+      let errorMessage: string = "Adding new camp failed!";
+      console.log(errorMessage);
     }
   };
 
@@ -31,22 +46,22 @@ const Details = () => {
       <Form className={classes.form}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Title</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" onChange={getInputDataHandler}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Location</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" onChange={getInputDataHandler}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Campground price</Form.Label>
-          <Form.Control type="number" />
+          <Form.Control type="number" onChange={getInputDataHandler}/>
         </Form.Group>
         <Form.Group className="mb-4" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Description</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" rows={3} onChange={getInputDataHandler}/>
         </Form.Group>
         <Form.Group controlId="formFile" className="mb-4">
-          <Form.Control type="file" />
+          <Form.Control type="file" onChange={getInputDataHandler}/>
         </Form.Group>
         <div className={classes.button}>
           <Button
@@ -54,7 +69,7 @@ const Details = () => {
             size="lg"
             type="button"
             className="mb-3"
-            onClick={newCampHandler}
+            onClick={getFormDataHandler}
           >
             Add campground
           </Button>
