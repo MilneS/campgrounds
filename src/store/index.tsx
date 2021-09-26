0b1a0657afc84c4cfad12b3a3755dc5882fa9828
Samira Milne <milne.samira@GMAIL.COM>
2021-09-26 18:43:55 -0500

    store cleanup

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -16,34 +16,25 @@ const reducerFunc = (state = initialState, action: Action) => {
   switch (action.type) {
     case "loginComp":
       return { ...state, showLogin: true, showSignup: false };
-      break;
     case "signupComp":
       return { ...state, showSignup: true, showLogin: false };
-      break;
     case "signupFormData":
       return { ...state, signUpFormData: action.payload };
-      break;
     case "loginFormData":
       return { ...state, loginFormData: action.payload };
-      break;
     case "loggedin":
       return { ...state, isLoggedin: true };
-      break;
     case "loggedout":
       return { ...state, isLoggedin: false };
-      break;
     case "logginFromCampsBtn":
       return { ...state, logginFromCamps: true };
-      break;
     case "logoutFromCampsBtn":
       return { ...state, logginFromCamps: false };
-      break;
     case "setAllCamps":
       return { ...state, allCamps: action.payload };
-      break;
     default:
       return state;
-      break;
+
   }
 };
 
