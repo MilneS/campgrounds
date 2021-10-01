392a7280019f54bc6324b5a8a7cb9b7e12a85e35
Samira Milne <milne.samira@GMAIL.COM>
2021-09-30 22:06:57 -0500

    Updated

--- src/firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-import * as  firebase from "firebase/app";
-import "firebase/storage";
-import "firebase/firebase-database";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIGS
-
-export const app = firebase.initializeApp(firebaseConfig);
-
